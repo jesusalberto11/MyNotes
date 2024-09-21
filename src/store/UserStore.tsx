e40e7e9b258126542d2557787d9a6eaec0ed263d
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-09-21 15:58:05 -0500

    Feat: Created userStore

---
+++ src/store/UserStore.tsx
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+interface IUserStoreState {
+  accessToken: string | null;
+  refreshToken: string | null;
+
+  setTokens: (acToken: string, rfToken: string) => void;
+
+  isLoggedIn: boolean | null;
+
+  logOut: () => void;
+}
+
+export const userUserStore = create<IUserStoreState>()((set) => ({
+  accessToken: null,
+  refreshToken: null,
+
+  isLoggedIn: null,
+
+  setTokens(acToken: string, rfToken: string) {
+    set({ accessToken: acToken, refreshToken: rfToken, isLoggedIn: true });
+  },
+
+  logOut() {
+    set({ accessToken: null, refreshToken: null, isLoggedIn: false });
+  },
+}));
