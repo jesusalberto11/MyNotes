2c673775198e54ea23eb65223c2fea68cb64d353
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-09-21 16:02:38 -0500

    Feat: Added method to check token exp time and logOut funcionality

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,17 +1,60 @@
 import { IUserData } from "../interfaces/IUserData";
 import { useAuthUser } from "../models/user/useAuthUser";
+import { userUserStore } from "../store/UserStore";
 
 export const useAuth = () => {
+  const { accessToken, isLoggedIn, setTokens, logOut } = userUserStore();
   const { authUser } = useAuthUser();
 
-  const isLoggedIn = false;
   const user: IUserData = {
     name: "Jesus Morales",
+  };
+
+  const checkForSavedToken = () => {
+    const savedToken = localStorage.getItem("token");
+    const refreshSavedToken = localStorage.getItem("refreshToken");
+
+    if (!savedToken || !refreshSavedToken) {
+      logOutUser();
+      console.log("El token se borró mi rey");
+
+      return;
+    }
+
+    const isTokenValid = verifyToken(savedToken);
+    const isRefreshTokenValid = verifyToken(refreshSavedToken);
+
+    if (!isTokenValid || !isRefreshTokenValid) {
+      logOutUser();
+      localStorage.removeItem("token");
+      localStorage.removeItem("refreshToken");
+      console.log("se venció el tiempo del token mi rey");
+
+      return;
+    }
+
+    setTokens(savedToken, refreshSavedToken);
+  };
+
+  const verifyToken = (token: any) => {
+    if (!token) return;
+
+    const decoded = JSON.parse(atob(token.split(".")[1]));
+    return decoded.exp * 1000 > Date.now();
+  };
+
+  const logOutUser = () => {
+    logOut();
+    localStorage.removeItem("token");
+    localStorage.removeItem("refreshToken");
   };
 
   return {
     isLoggedIn,
     user,
     authUser,
+    logOutUser,
+    accessToken,
+    checkForSavedToken,
   };
 };
